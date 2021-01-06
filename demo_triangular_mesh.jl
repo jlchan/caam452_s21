89621ed5da49771029b901faf5c55699124e13cf
Jesse Chan <jesse.chan@rice.edu>
2021-01-05 22:53:16 -0600

    initial commit

---
+++ demo_triangular_mesh.jl
@@ -0,0 +1,40 @@
+using TriangleMesh
+using Plots
+using Triplot
+
+import GR
+
+poly = polygon_regular(5)
+# poly = polygon_Lshape()
+mesh = create_mesh(poly, set_area_max=true, quality_meshing=true)
+
+function plotTriMesh(mesh)
+    x,y = mesh.point[1,:], mesh.point[2,:]
+
+    plot() # init plot
+    for vertex_ids in eachcol(mesh.cell)
+        ids = vcat(vertex_ids, vertex_ids[1])
+        plot!(x[ids],y[ids],linecolor=:black)
+    end
+    display(plot!(legend=false,ratio=1))
+end
+plotTriMesh(mesh)
+
+x,y = mesh.point[1,:], mesh.point[2,:]
+z,t = (@. sin(pi*x)*sin(pi*y)),mesh.cell
+
+# GR.beginprint("test.png")
+# GR.setcolormap(1)
+# tripcolor(x,y,z,t)
+# GR.endprint()
+
+function to_rgba(x::UInt32)
+    a = ((x & 0xff000000)>>24)/255
+    b = ((x & 0x00ff0000)>>16)/255
+    g = ((x & 0x0000ff00)>>8)/255
+    r = (x & 0x000000ff)/255
+    RGBA(r, g, b, a)
+end
+
+img = to_rgba.(Triplot.rasterize(x,y,z,t)')
+plot([extrema(x)...], [extrema(y)...], img, yflip=false)
---
+++ pluto_demo.jl
@@ -0,0 +1,159 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+# ╔═╡ 91615676-4eb1-11eb-2599-9f94046c2a5d
+begin
+	let
+		env = mktempdir()
+		import Pkg
+		Pkg.activate(env)
+		Pkg.Registry.update()
+		Pkg.add(Pkg.PackageSpec(;name="PlutoUI", version="0.6.7-0.6"))
+	end
+	using PlutoUI
+end
+
+# ╔═╡ f381d0ce-3faf-11eb-2586-ffe002ea8d9d
+begin 
+	using Plots
+	using LinearAlgebra
+	using SparseArrays
+end
+
+# ╔═╡ 6ce54252-3fb0-11eb-34e6-978f07a94d99
+md"""
+
+# Simulation of a model PDE 
+
+Let's try to build some intuition for how a PDE behaves. Suppose we have a PDE on ``[-1,1]``
+
+```math
+\begin{gather*}
+\frac{\partial u}{\partial t} + a\frac{\partial u}{\partial x} - b\frac{\partial^2 u}{\partial x^2} = f(x,t)
+\end{gather*}
+```
+with left and right boundary conditions
+```math
+u(-1,t) = u_L(t), \qquad u'(1,t) = 0
+```
+and initial condition ``u(x,0) = u_0(x)``.
+
+
+Different choices of ``a,b,c`` transition between different types of PDEs. 
+
+## Physical parameters
+
+Try choosing different values for each parameter. 
+
+``a =`` $(@bind a NumberField(0:.1:10, default = 1))
+``b =`` $(@bind b NumberField(0:.1:10, default = 1))
+
+Different parameter values can yield different PDEs. For example:
+- ``a=1, b = 0`` results in the advection equation
+- ``a=0, b = 1`` results in the heat equation 
+"""
+
+# ╔═╡ e6a60308-3fb8-11eb-090f-9d533d92759b
+md"Let's now define a forcing function `f(x,t)`, left boundary data `uL(t)`, and an initial condition `u0(x)`. Feel free to change these (set them to zero to remove)."
+
+# ╔═╡ eb538830-3fb8-11eb-1d93-230874209dc7
+begin
+	u0(x)  = (.5*sin(pi*x) - .25*sin(2*pi*x) + .25*sin(4*pi*x))
+	f(x,t) = 0*(exp(-100*(x+.75)^2))*(.5*t + sin(2*t))*exp(-.1*t)
+	uL(t)  = 0*.75*sin(1*pi*t)
+end;
+
+# ╔═╡ 1afcd228-3fc1-11eb-127a-b5059e1a0029
+md"""
+# Discretization parameters
+"""
+
+# ╔═╡ 952ca744-3fb7-11eb-20ab-a90257510dfb
+md"""
+Number of nodes `N` and timestep `dt`
+
+`N =` $(@bind N NumberField(3:2:200, default = 101))
+
+`dt =` $(@bind dt NumberField(.0001:.0001:10, default = .01))
+
+Final time = $(@bind FinalTime NumberField(0:.0001:10, default = .25))
+"""
+
+# ╔═╡ 9a758898-3fd4-11eb-0718-752c99372d54
+md"Construction of discretization matrices"
+
+# ╔═╡ d323417c-3fb5-11eb-0cdb-655211612ff0
+begin 
+	x = LinRange(-1,1,N)
+	h = 2/(N-1) # mesh size
+	K = spdiagm(-1=>-ones(N-1),1=>-ones(N-1),0=>2*ones(N))
+	K[1,1] = -1  
+	K[N,N] = 1 # corrections at the left/right endpoints
+	K = (@. K/h^2)
+		
+	Q = spdiagm(-1=>-ones(N-1),1=>ones(N-1))
+	Q[end,end] = 1
+	Q = (@. Q/h)
+	
+	# modify matrices for Dirichlet BCs
+	K[1,:] .= 0
+	K[1,1] = 1
+	Q[1,:] .= 0
+	Q[1,1] = 1
+end;
+
+# ╔═╡ 3b1b2ad0-3fbe-11eb-0745-2f8aeccfea54
+md"Now we'll solve our PDE until final time $(FinalTime) and animate the results."
+
+# ╔═╡ 4b1df9c6-3fbe-11eb-1dcf-3f883d7d92de
+begin
+	B = (a*Q + b*K) 
+	A = I + .5*dt*B # Crank-Nicholson in time
+	Nsteps = ceil(Int,FinalTime/dt)
+	u = u0.(x) 
+	
+	anim = @animate for i = 1:Nsteps
+		global u
+		t = i*dt
+
+		bb = u + dt*f.(x,t) - .5*dt*B*u 
+		bb[1] = uL(t) # impose BC at left endpoint
+		u = A\bb
+
+		plot(x,u,xlim=(-1,1),ylim=(-1,1),lw=3,label="Solution")
+		scatter!(x,u,label="")
+		plot!(title="Time=$t",ratio=1)
+	end	every 1
+	gif(anim, "fem.gif", fps = 10)
+end
+
+# ╔═╡ d9813424-3fb1-11eb-2f61-0b5abf1991fe
+md"""
+# Package loading code
+"""
+
+# ╔═╡ Cell order:
+# ╟─6ce54252-3fb0-11eb-34e6-978f07a94d99
+# ╟─e6a60308-3fb8-11eb-090f-9d533d92759b
+# ╠═eb538830-3fb8-11eb-1d93-230874209dc7
+# ╟─1afcd228-3fc1-11eb-127a-b5059e1a0029
+# ╟─952ca744-3fb7-11eb-20ab-a90257510dfb
+# ╟─9a758898-3fd4-11eb-0718-752c99372d54
+# ╠═d323417c-3fb5-11eb-0cdb-655211612ff0
+# ╟─3b1b2ad0-3fbe-11eb-0745-2f8aeccfea54
+# ╠═4b1df9c6-3fbe-11eb-1dcf-3f883d7d92de
+# ╟─d9813424-3fb1-11eb-2f61-0b5abf1991fe
+# ╟─91615676-4eb1-11eb-2599-9f94046c2a5d
+# ╟─f381d0ce-3faf-11eb-2586-ffe002ea8d9d
