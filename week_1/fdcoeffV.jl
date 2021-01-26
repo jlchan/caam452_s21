53ff9c7dd548e1a35b1f8bfb9893380ada1848b9
Jesse Chan <jchan985@gmail.com>
2021-01-25 21:59:00 -0600

    update fdcoeffV

--- week_1/fdcoeffV.jl
+++ week_1/fdcoeffV.jl
@@ -3,10 +3,9 @@ function fdcoeffV(k,x̄,x)
     if n <= k
         error("Too few points in the stencil")
     end
-    A = ones(n,n)
-    xrow = (x .- x̄)
+    A = ones(n,n)    
     for i = 2:n
-        A[i,:] = @. xrow ^(i-1) / factorial(i-1)
+        A[i,:] = @. (x - x̄)^(i-1) / factorial(i-1)
     end
     b = zeros(n,1)
     b[k+1] = 1
