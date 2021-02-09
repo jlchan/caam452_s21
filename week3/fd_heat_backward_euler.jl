82d3b8790dd60739074e4e77e10530d9ecfbfbd6
Jesse Chan <jchan985@gmail.com>
2021-02-09 13:55:34 -0600

    update

--- week3/fd_heat_backward_euler.jl
+++ week3/fd_heat_backward_euler.jl
@@ -3,31 +3,38 @@ using SparseArrays
 using Plots
 
 m = 100 # number of points
-FinalTime = 1.0
+T = 1.0
 dt = .01
 
+u0(x) = 0.0 # initial condition
+f(x) = 5*(Float64((x > -.5) && (x <= 0.0)) - Float64((x < .5) && (x >= 0.0)))
+f(x,t) = f(x)*exp(-t)
+α(t) = 1.0
+β(t) = pi
+
+# define spatial grid
 x = LinRange(-1,1,m+2)
 xint = x[2:end-1]
-
-u0(x) = 0.0
-f(x) = 10*(Float64((x > -.5) && (x <= 0.0)) - Float64((x < .5) && (x >= 0.0)))
-f(x,t) = f(x)*exp(-t)
-α,β = 1.0,pi
-
 h = x[2]-x[1]
 A = (1/h^2) * spdiagm(0=>2*ones(m),-1=>-ones(m-1),1=>-ones(m-1))
-b = f.(xint)
-b[1] += α/h^2
-b[m] += β/h^2
+
+function F(t)
+    b = f.(xint,t) # look for f(x,t)
+    b[1] += α(t)/h^2
+    b[end] += β(t)/h^2
+    return b
+end
 
 u = u0.(xint)
-Nsteps = ceil(Int,FinalTime/dt)
-interval = 1
+Nsteps = ceil(Int,T/dt)
+dt = T / Nsteps
+
+interval = 2
 @gif for i = 1:Nsteps
     t = i*dt
-    u .= (I + dt*A)\(@. u + dt*f(xint,t))
-    if i%interval==0
-        plot(xint,u,linewidth=2,label="Solution",ylims=(-1.0,1.0))
+    u .= (I + dt*A)\(u .+ dt*F(t))
+    if i % interval==0
+        plot(xint,u,linewidth=2,label="Solution",ylims=(-1.0,3.0))
         println("on timestep $i out of $Nsteps.")
     end
 end every interval
